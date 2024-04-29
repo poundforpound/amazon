2540778a0f022a9bf041de88b70c1473cead5729
PoundforPound <a.zenit2014@yandex.ru>
2024-04-29 15:57:46 +0300

    Move addProductToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addProductToCart(productId) {
+  let matchingItem;
+  cart.forEach((item) => {
+    if (item.productId === productId) {
+      matchingItem = item;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId,
+      quantity: 1,
+    });
+  }
+}
